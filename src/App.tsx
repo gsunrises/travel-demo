a1145ac95cebb39e720979b48c47607b09db58c4
gsunrises <52069186+gsunrises@users.noreply.github.com>
2023-02-28 22:19:26 +0800

    commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './App.module.css';
+import { Layout, Typography, Input, Button, Menu, Dropdown } from 'antd';
+import { GlobalOutlined } from '@ant-design/icons'
+function App() {
+    return (
+        <div className={styles.App}>
+            <div>
+                <Typography.Text>
+                    让旅游更幸福
+                </Typography.Text>
+                <Dropdown.Button
+                    overlay={
+                        <Menu
+                            items={
+                                [{ key: 1, label: '中文' },
+                                { key: 2, label: 'English' }]
+                            }
+                        />
+                    }
+                    icon={<GlobalOutlined/>}
+                >
+                    语言
+                </Dropdown.Button>
+            </div>
+            <div className={styles["App-header"]}>
+                <Layout.Header className={styles["main-header"]}>
+                    <img src={logo} alt="" className={styles["App-logo"]} />
+                    <Typography.Title level={3} className={styles.title}>
+                        React 旅游网
+                    </Typography.Title>
+                    <Input.Search
+                        placeholder={'请输入目的地、主题'}
+                        className={styles["search-input"]}
+                    ></Input.Search>
+                </Layout.Header>
+            </div>
+        </div>
+    );
+}
+
+export default App;
